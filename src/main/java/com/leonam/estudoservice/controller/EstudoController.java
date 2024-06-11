f8a781b07ab50754884c57bac2d8047a1ca18262
leonamlucius <leonam253@gmail.com>
2024-06-11 13:49:34 -0300

    POST e GET

--- src/main/java/com/leonam/estudoservice/controller/EstudoController.java
+++ src/main/java/com/leonam/estudoservice/controller/EstudoController.java
@@ -3,7 +3,6 @@ package com.leonam.estudoservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,29 +10,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonam.estudoservice.model.Estudo;
-import com.leonam.estudoservice.repository.EstudosRepository;
 import com.leonam.estudoservice.services.EstudosService;
 
 @RestController
 @RequestMapping("/estudo")
 public class EstudoController {
 
-	
-	
 	@Autowired
-	private EstudosService services;
-	
-	
+	private EstudosService estudosservices;
 
+	@GetMapping("/get")
+	public List<Estudo> findAll() {
 
-	@GetMapping("/teste")
-	public List<Estudo> getInf(){
+		return estudosservices.findAll();
+	}
+
+	@PostMapping("/post")
+	public Estudo post(@RequestBody Estudo estudo) {
 		
-		
-		return services.findAll();
+		return estudosservices.post(estudo);
 		
 	}
-	
-	
 
 }
--- src/main/java/com/leonam/estudoservice/services/EstudosService.java
+++ src/main/java/com/leonam/estudoservice/services/EstudosService.java
@@ -3,24 +3,37 @@ package com.leonam.estudoservice.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.leonam.estudoservice.model.Estudo;
 import com.leonam.estudoservice.repository.EstudosRepository;
 
-
+@Service
 public class EstudosService {
-	
+
 	@Autowired
 	private EstudosRepository estudorepository;
-	
+
 	public Estudo save(Estudo estudo) {
-		
+
 		return estudorepository.save(estudo);
 	}
-	
-	public List<Estudo> findAll(){
+
+	public List<Estudo> findAll() {
+
+		List<Estudo> estudo = estudorepository.findAll();
+
+		return estudo;
+
+	}
+
+	public Estudo post(@RequestBody Estudo estudo) {
 		
-		return estudorepository.findAll();
+		estudo =  estudorepository.save(estudo);
+		
+		return estudo;
+
 	}
 
 }
